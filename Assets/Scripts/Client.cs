6c5a32d40ff3e81e5f1b4f03f769f252f566d99c
Spark <Muammar.Yacoob@gmail.com>
2020-01-24 11:46:16 +0100

    Scripts

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Client : MonoBehaviour
+{
+    #region netwrok variables
+    private const int MAX_CONNECTIONS = 100;
+    private const string SERVER_IP = "127.0.0.1";
+    private const int SERVER_WIN_PORT = 8999;
+    private const int SERVER_WEB_PORT = 8998;
+    private const int BUFFER_SIZE = 1024;
+
+    private byte[] buffer = new byte[BUFFER_SIZE];
+    private int winHostID;
+
+    private QosType channelType;
+    private byte err;
+    #endregion
+
+    private int connectionID = -1;
+    private bool isConnected;
+
+    private void Start()
+    {
+        GlobalConfig globalConfig = new GlobalConfig();
+        NetworkTransport.Init(globalConfig);
+
+        //host topology
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+        connectionConfig.AddChannel(QosType.Unreliable);
+
+        HostTopology hostTopo = new HostTopology(connectionConfig, MAX_CONNECTIONS);
+
+        //connecting to host
+        winHostID = NetworkTransport.AddHost(hostTopo, 0);
+        channelType = QosType.Unreliable;
+
+#if UNITY_WEBGL
+        connectionID = NetworkTransport.Connect(winHostID:, SERVER_IP, SERVER_WEB_PORT, 0, out err);
+#else
+        connectionID = NetworkTransport.Connect(winHostID, SERVER_IP, SERVER_WIN_PORT, 0, out err);
+#endif
+        if (connectionID >= 0)
+            isConnected = true;
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+    #region netwrok variables
+    protected const int MAX_CONNECTIONS = 100;
+    protected const string SERVER_IP = "127.0.0.1";
+    protected const int SERVER_WIN_PORT = 8999;
+    protected const int SERVER_WEB_PORT = 8998;
+    protected const int BUFFER_SIZE = 1024;
+    protected byte[] buffer = new byte[BUFFER_SIZE];
+    protected int winHostID;
+    protected int webHostID;
+    protected QosType channelType;
+    protected byte err;
+    #endregion
+
+    private bool isInitialized;
+    private void Start()
+    {
+
+        GlobalConfig globalConfig = new GlobalConfig();
+        NetworkTransport.Init(globalConfig);
+
+        //host topology
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+        connectionConfig.AddChannel(QosType.Unreliable);
+
+        HostTopology hostTopo = new HostTopology(connectionConfig, MAX_CONNECTIONS);
+
+        //adding hosts
+        winHostID = NetworkTransport.AddHost(hostTopo, SERVER_WIN_PORT);
+        webHostID = NetworkTransport.AddWebsocketHost(hostTopo, SERVER_WEB_PORT);
+
+        channelType = QosType.Unreliable;
+
+        isInitialized = true;
+    }
+
+
+    private void Update()
+    {
+        if (!isInitialized)
+            return;
+
+        int outHostID, outConnectionID, outChannelID;
+        int receivedSize;
+
+
+        NetworkEventType netEvent = NetworkTransport.Receive(out outHostID, out outConnectionID, out outChannelID, buffer, buffer.Length, out receivedSize, out err);
+
+        if (netEvent == NetworkEventType.Nothing)
+            return;
+
+        switch (netEvent)
+        {
+            case NetworkEventType.ConnectEvent:
+                Debug.Log($"Connection from: {outConnectionID}, through channel: {outChannelID}");
+                break;
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log($"Disconnected from: {outConnectionID}, through channel: {outChannelID}");
+                break;
+            case NetworkEventType.DataEvent:
+                Debug.Log($"Data from: {outConnectionID}, through channel: {outChannelID}, Message: {System.Text.Encoding.UTF8.GetString(buffer)}");
+                break;
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.BroadcastEvent:
+                break;
+        }
+
+    }
+
+    private void sendToClient()
+    {
+        NetworkTransport.Send(1,)
+    }
+
+}
+
+
+
+public abstract class NetworkSocket
+{
+    protected const int MAX_CONNECTIONS = 100;
+    protected const string SERVER_IP = "127.0.0.1";
+    protected const int SERVER_WIN_PORT = 8999;
+    protected const int SERVER_WEB_PORT = 8998;
+    protected const int BUFFER_SIZE = 1024;
+    protected byte[] buffer = new byte[BUFFER_SIZE];
+    protected int winHostID;
+    protected int webHostID;
+    protected QosType channelType;
+    protected byte err;
+}
